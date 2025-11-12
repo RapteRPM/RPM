c6816727df08be8189b0b12fbf4af02d02e8c096
David Sebastian Duarte Rojas <147888454+RapteRPM@users.noreply.github.com>
2025-11-12 02:30:40 +0000

    Agregar servidor Python personalizado para Railway

---
+++ server.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import http.server
+import socketserver
+import os
+
+PORT = int(os.environ.get('PORT', 8000))
+DIRECTORY = "."
+
+class MyHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, directory=DIRECTORY, **kwargs)
+
+    def end_headers(self):
+        self.send_header('Cache-Control', 'no-store, no-cache, must-revalidate')
+        super().end_headers()
+
+with socketserver.TCPServer(("0.0.0.0", PORT), MyHTTPRequestHandler) as httpd:
+    print(f"Server running at http://0.0.0.0:{PORT}/")
+    httpd.serve_forever()
